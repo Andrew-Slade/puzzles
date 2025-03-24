db77a153b3b93472837ac123e1f8b8df3de8c130
Andrew Slade <28659770+Andrew-Slade@users.noreply.github.com>
2025-03-23 21:04:31 -0500

    Relatively more efficient method

--- TwoSum.cc
+++ TwoSum.cc
@@ -12,41 +12,31 @@ You can return the answer in any order.
 #include <iostream>
 using namespace std;
 
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+*/
 class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
-            vector<int> idx;
-            vector<pair<int, int>> order;
-            for (int i = 0; i < nums.size(); i++) {
-                order.push_back({i, nums[i]});
-            }
-            sort(order.begin(), order.end(),
-                 [](auto a, auto b) { return a.second < b.second; });
-            int sum = 0;
-            int mindx = 0;
-            int maxdx = order.size() - 1;
-            for (int i = 0; i < order.size(); i++){
-                int sum = order.at(mindx).second + order.at(maxdx).second;
-                if( sum > target){
-                    maxdx--;
-                }else if(sum < target){
-                    mindx++;
-                }if(sum == target){
-                    idx = {order.at(mindx).first, order.at(maxdx).first};
-                    return idx;
+            int s = nums.size();
+            for(int i = 0; i < s; ++i){
+                nums[i] = target - nums[i];
+                int t = nums[i];
+                for(int j = s - 1; j > i; --j){
+                    if(nums[j] == t){
+                        nums.clear();
+                        nums.push_back(j);
+                        nums.push_back(i);
+                        return nums;
+                    }
                 }
-            }
-         return idx;
+             }
+            return nums;
         }
     };
-
-    int main(){
-        vector<int> n = {4,6,2,7,89,1};
-        int t = 11;
-        Solution s = Solution();
-        vector<int> res = s.twoSum(n, t);
-        for(auto i: res){
-            cout << i << endl;
-        }
-        return 0;
-    }
