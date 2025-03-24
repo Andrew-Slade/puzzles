683e83b5079ecd69f36132a546e47e5074c3613b
Andrew Slade <28659770+Andrew-Slade@users.noreply.github.com>
2025-03-23 19:59:29 -0500

    Removed some space inefficiencies

--- LongestCommonPrefix.cc
+++ LongestCommonPrefix.cc
@@ -5,27 +5,18 @@ using namespace std;
 class Solution {
     public:
         string longestCommonPrefix(vector<string>& strs) {
-            string res;
-            int idx = 0;
-            bool pastFirst = false;
-            for(auto i: strs){
-                for(int j = 0; j < max(i.length(), res.length()); ++j){
-                    if(!pastFirst){
-                        res = i;
-                        break;
+            string res = strs[0];
+            strs.erase(strs.begin());
+            for(string i: strs){
+                for(int j = 0; j < res.length(); ++j){
+                    if(res[j] == i[j]){
+                        res[j] = i[j];
                     }else{
-                        if(res[j] == i[j]){
-                            res[j] = i[j];
-                        }else{
-                            res = res.substr(0,j);
-                            break;
-                        }
+                        res = res.substr(0,j);
+                        break;
                     }
                 }
-                pastFirst = true;
             }
-            
-
             return res;
         }
     };
